7d57abbd47814dc41e2f0f8300c43ab9adbcf33e
maximilian <maximilian@maximilian-PC>
2012-09-20 17:55:33 +0200

    beta version

--- app/code/community/Qshops/Tooltip/Block/AddTooltip.php
+++ app/code/community/Qshops/Tooltip/Block/AddTooltip.php
@@ -21,39 +21,43 @@ class Qshops_Tooltip_Block_AddTooltip extends Mage_Core_Block_Text
 
             }
             fclose($handle);
+        
+            $css = '
+                <style>
+                    /* Tooltip */
+
+                    .qshops-tooltip {
+                        position:relative;
+                        z-index:24; 
+                            border-bottom: 2px dotted #555;
+                    }
+
+                    .qshops-tooltip:hover{ z-index:25; }
+
+                    .qshops-tooltip span{ display: none }
+
+                    .qshops-tooltip:hover span {
+                        display:block;
+                        position:absolute;
+                        bottom:2em; left:2em; width:15em;
+                        border:1px solid #000;
+                            border-radius: 5px;
+                            box-shadow: 4px 4px 4px rgba(0,0,0,0.5);
+                        background-color: #000; 
+                        background-color: rgba(0,0,0,0.8); 
+                            color:#fff;
+                            font-weight: normal;
+                            text-align: left;
+                            padding: 5px;
+                    }
+
+                </style>
+            ';
+            return $transport.$css;
         }
-        $css = '
-            <style>
-                /* Tooltip */
-
-                .qshops-tooltip {
-                    position:relative;
-                    z-index:24; 
-                        border-bottom: 2px dotted #555;
-                }
-
-                .qshops-tooltip:hover{ z-index:25; }
-
-                .qshops-tooltip span{ display: none }
-
-                .qshops-tooltip:hover span {
-                    display:block;
-                    position:absolute;
-                    bottom:2em; left:2em; width:15em;
-                    border:1px solid #000;
-                        border-radius: 5px;
-                        box-shadow: 4px 4px 4px rgba(0,0,0,0.5);
-                    background-color: #000; 
-                    background-color: rgba(0,0,0,0.8); 
-                        color:#fff;
-                        font-weight: normal;
-                        text-align: left;
-                        padding: 5px;
-                }
-                
-            </style>
-        ';
-        return $transport.$css;
+        else {
+            return $transport;
+        }
     }
 }
 ?>
